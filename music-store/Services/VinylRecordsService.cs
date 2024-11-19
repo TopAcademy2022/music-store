c32a82ae409fa748804c0ea91331b2a7bd23d3b2
Igor <igor.kasyanov00@mail.ru>
2024-11-19 21:42:15 +0300

    Added method for adding vinyl records. #3

---
+++ music-store/Services/VinylRecordsService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using music_store.Services.Interfaces;
+using music_store.Models.Entities;
+
+namespace music_store.Services
+{
+	public class VinylRecordsService : IVinylRecordsService
+	{
+		private ADatabaseConnection _dbConnection; //!< Database connection
+
+		public VinylRecordsService()
+		{
+			this._dbConnection = new SqliteConnection();
+		}
+
+		/*!
+		* @brief Adding plates to our database.
+		* @param[in] vinylRecord class instance to add.
+		* @return True - vinylRecord  added; False - vinylRecord not added.
+		*/
+		public bool AddVinilRecord(VinylRecord vinylRecord)
+		{
+			try
+			{
+				this._dbConnection.VinylRecords.Add(vinylRecord); //!< Adding a change to our data
+				this._dbConnection.SaveChanges(); //!< Save changes to our data
+
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message); //!< Exception output
+			}
+
+			return false;
+		}
+	}
+}
