04605c889fbe17053749e3e7f672d6bc3897ac50
Igor <igor.kasyanov00@mail.ru>
2024-11-19 21:42:15 +0300

    Added method for adding vinyl records. Closed #3

---
+++ music-store/Services/Interfaces/IVinylRecordsService.cs
@@ -0,0 +1,12 @@
+﻿using music_store.Models.Entities;
+
+namespace music_store.Services.Interfaces
+{
+	public interface IVinylRecordsService
+	{
+		/*! 
+		* @brief Adding plates to our database.
+		*/
+		public bool AddVinilRecord(VinylRecord vinylRecord);
+	}
+}
