d2df17e7bebfc00b2b0ce583f1557ace9d88edc4
Viktor080181 <lozhnvikt0801@mail.ru>
2024-11-21 20:52:46 +0300

    Added method for adding music publishers #5

---
+++ music-store/Services/MusicPublisherService.cs
@@ -0,0 +1,41 @@
+﻿using music_store.Models.Entities;
+using music_store.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace music_store.Services
+{
+    /*! 
+       * @brief Add method for adding music publishers.
+       */
+    internal class MusicPublisherService:ImusicPublisherService
+    {
+
+        private ADatabaseConnection _databaseConnection;
+
+        public MusicPublisherService(ADatabaseConnection aDatabaseConnection)
+        {
+            this._databaseConnection = aDatabaseConnection;
+        }
+
+        public bool AddMusicPublisher(MusicPublisher musicPublisher)
+        {
+            try
+            {
+                this._databaseConnection.MusicPublishers.Add(musicPublisher);
+                this._databaseConnection.SaveChanges();
+
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+
+            return false;
+        }
+    }
+
+}
+}
