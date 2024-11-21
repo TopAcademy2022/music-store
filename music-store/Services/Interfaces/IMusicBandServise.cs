e77488a4c9db0ba2a7237be2906349915d9ba161
natalia21082000 <161637129+natalia21082000@users.noreply.github.com>
2024-11-21 21:16:59 +0300

    Added method for adding music bands. Closed #4

--- music-store/Services/Interfaces/IMusicBandServise.cs
+++ music-store/Services/Interfaces/IMusicBandServise.cs
@@ -2,18 +2,12 @@
 
 namespace music_store.Services.Interfaces
 {
-	/**
+	/*!
 	 * @interface IMusicBand
 	 * @brief Interface for working with music bands.
 	 */
 	public interface IMusicBandServise
 	{
-		/**
-		 * @brief Adds a music band.
-		 *
-		 * @param musicBand The music band object to be added.
-		 * @return True if the operation is successful, otherwise False.
-		 */
 		public bool AddMusicBand(MusicBand musicBand);
 	}
 }
