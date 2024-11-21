07fcd6c105b1cc256f25a75a41a6f99d1a5d1a86
natalia21082000 <161637129+natalia21082000@users.noreply.github.com>
2024-11-21 20:15:48 +0300

    Added method for adding music bands. Closed #4

---
+++ music-store/Services/Interfaces/IMusicBandServise.cs
@@ -0,0 +1,19 @@
+﻿using music_store.Models.Entities;
+
+namespace music_store.Services.Interfaces
+{
+	/**
+	 * @interface IMusicBand
+	 * @brief Interface for working with music bands.
+	 */
+	public interface IMusicBandServise
+	{
+		/**
+		 * @brief Adds a music band.
+		 *
+		 * @param musicBand The music band object to be added.
+		 * @return True if the operation is successful, otherwise False.
+		 */
+		public bool AddMusicBand(MusicBand musicBand);
+	}
+}
---
+++ music-store/Services/MusicBandService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using music_store.Models.Entities;
+using music_store.Services.Interfaces;
+
+namespace music_store.Services
+{
+	/**
+	 * @class ServiceMusicBand
+	 * @brief Class for working with music bands.
+	 */
+	public class ServiceMusicBand
+	{
+		/**
+		 * @brief Method to add a music band.
+		 * @param musicBand The music band object.
+		 * @return True if the addition was successful;False otherwise .
+		 */
+		public bool AddMusicBand(MusicBand musicBand)
+		{
+			try
+			{
+				ADatabaseConnection db = new SqliteConnection();
+				db.MusicBands.Add(musicBand);
+				db.SaveChanges();
+
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+
+			return false;
+		}
+	}
+}
