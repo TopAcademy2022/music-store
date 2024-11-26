bf02ea692675f6e51f7aa137546f9547c28cf6b4
global133 <161868236+global133@users.noreply.github.com>
2024-11-26 19:40:56 +0300

    Update IMusicGenereService.cs

--- music-store/Services/Interfaces/IMusicGenereService.cs
+++ music-store/Services/Interfaces/IMusicGenereService.cs
@@ -5,7 +5,11 @@ namespace music_store.Services.Interfaces
 {
     public interface IMusicGenereService
     {
+        /*! 
+		* @brief Adding genres to our database.
+        * @param[in] MusicGenre genere - class instance to add.
+        * @return True - music genere added; False - music genere not added.
+        */
         public bool AddMusicGenre(MusicGenre genere);
-
     }
 }
