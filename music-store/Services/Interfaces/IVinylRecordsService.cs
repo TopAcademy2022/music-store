b2093f81852999065cfea9eb122058a831613cd7
natalia21082000 <161637129+natalia21082000@users.noreply.github.com>
2024-11-26 21:00:50 +0300

    Added method for find vinyl records by name. Closed #23

--- music-store/Services/Interfaces/IVinylRecordsService.cs
+++ music-store/Services/Interfaces/IVinylRecordsService.cs
@@ -13,7 +13,7 @@ namespace music_store.Services.Interfaces
 
 
         /*! \fn VinylRecord? SearchByName(string vinylRecordName)
-       *  \brief Searches for a vinyl record by its name.
+       *  @brief Searches for a vinyl record by its name.
        *  \param[in] vinylRecordName The name of the vinyl record to search for.
        *  \return The first matching vinyl record found, or null if no match is found.
        */
