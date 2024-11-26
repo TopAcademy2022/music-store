e14cbdc180b9e373ee9d0b00902479cd7eefc886
global133 <161868236+global133@users.noreply.github.com>
2024-11-26 19:42:53 +0300

    Update MusicGenereService.cs

--- music-store/Services/MusicGenereService.cs
+++ music-store/Services/MusicGenereService.cs
@@ -2,17 +2,17 @@
 using music_store.Models.Entities;
 using music_store.Services.Interfaces;
 
-
 namespace music_store.Services
 {
     public class MusicGenereService : IMusicGenereService
     {
-        private ADatabaseConnection _DatabaseConnection;
+        private ADatabaseConnection _databaseConnection;
 
         public MusicGenereService(ADatabaseConnection databaseConnection)
         {
             this._DatabaseConnection = databaseConnection;
         }
+        
         public bool AddMusicGenre(MusicGenre genere)
         {
             try
