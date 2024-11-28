5f63c2dedfaf4e950d80cfa57f30f606f2bf1c64
natalia21082000 <161637129+natalia21082000@users.noreply.github.com>
2024-11-28 22:42:31 +0300

    Added password hashing. Closed #34

--- music-store/Services/Interfaces/IUserService.cs
+++ music-store/Services/Interfaces/IUserService.cs
@@ -9,5 +9,12 @@ namespace music_store.Services.Interfaces
 		public User? FindUser(User user);
 
 		public bool DeleteUser(User user);
+
+		/*! 
+		 * @brief Hashes a password with SHA3-512 and appends a predefined salt, then hashes the resulting string.
+		 * @param[in] password The plain text password to be hashed.
+		 * @return The final hashed password as a hexadecimal string.
+		 */
+		public string HashString(string password);
 	}
 }
--- music-store/Services/UserService.cs
+++ music-store/Services/UserService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
+using Org.BouncyCastle.Crypto.Digests;
 using music_store.Models.Entities;
 using music_store.Services.Interfaces;
 
@@ -8,6 +10,8 @@ namespace music_store.Services
 	public class UserService : IUserService
 	{
 		private ADatabaseConnection _databaseConnection;
+
+		private const string _salt = "34KFEB4466JVVNN443JJ25J66564J4N685NN";
 
 		public UserService(ADatabaseConnection aDatabaseConnection)
 		{
@@ -40,7 +44,7 @@ namespace music_store.Services
 		{
 			try
 			{
-				return this._databaseConnection.Users.Where(usr => 
+				return this._databaseConnection.Users.Where(usr =>
 				usr.Login == user.Login &&
 				usr.Password == user.Password).FirstOrDefault();
 			}
@@ -72,6 +76,32 @@ namespace music_store.Services
 			}
 
 			return false;
+		}
+
+		public string HashString(string password)
+		{
+			var sha3 = new Sha3Digest(512);
+
+			byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+			sha3.BlockUpdate(passwordBytes, 0, passwordBytes.Length);
+			byte[] hashBytes = new byte[sha3.GetDigestSize()];
+			sha3.DoFinal(hashBytes, 0);
+
+			var saltedPasswordBytes = new byte[hashBytes.Length + Encoding.UTF8.GetByteCount(_salt)];
+			Buffer.BlockCopy(hashBytes, 0, saltedPasswordBytes, 0, hashBytes.Length);
+			Buffer.BlockCopy(Encoding.UTF8.GetBytes(_salt), 0, saltedPasswordBytes, hashBytes.Length, Encoding.UTF8.GetByteCount(_salt));
+
+			sha3.BlockUpdate(saltedPasswordBytes, 0, saltedPasswordBytes.Length);
+			byte[] finalHashBytes = new byte[sha3.GetDigestSize()];
+			sha3.DoFinal(finalHashBytes, 0);
+
+			var finalHashString = new StringBuilder(finalHashBytes.Length * 2);
+			foreach (byte b in finalHashBytes)
+			{
+				finalHashString.AppendFormat("{0:x2}", b);
+			}
+			return finalHashString.ToString();
 		}
 	}
 }
